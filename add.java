a8991e876dd65b48a33c5a6387b19e3cb40de7df
obulreddy <106804920+Obulreddy0@users.noreply.github.com>
2023-05-31 10:37:16 +0530

    add.java
    Adding two numbers in java

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class add{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter two numbers:");
+int x=s.nextInt();
+int y=s.nextInt();
+int result=x+y;
+System.out.println(result);
+}
+}
